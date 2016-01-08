001b72ca2c4720b68b0f421ed96f8c247c4965bf
Nick Johnstone <ncwjohnstone@gmail.com>
2016-01-08 18:42:25 +1300

    Display projects!

--- data/projects.js
+++ data/projects.js
@@ -1,8 +1,9 @@
 export default [
   {
-    name: "RxMarbles",
-    description: "Interactive diagrams of Rx Observables",
-    repository: "https://github.com/staltz/rxmarbles",
-    homepage: "http://rxmarbles.com/"
+    name: 'RxMarbles',
+    description: 'Interactive diagrams of Rx Observables',
+    repository: 'https://github.com/staltz/rxmarbles',
+    homepage: 'http://rxmarbles.com/',
+    screenshot: 'data/images/rxmarbles.png'
   }
 ]
