bacb945119b61f6e0833dbe9e74d58e80acd5021
Raquel Moss <raquelxmoss@gmail.com>
2016-01-08 18:52:00 +1300

    Add ghostwriter

--- data/projects.js
+++ data/projects.js
@@ -1,8 +1,16 @@
 export default [
   {
-    name: "RxMarbles",
-    description: "Interactive diagrams of Rx Observables",
-    repository: "https://github.com/staltz/rxmarbles",
-    homepage: "http://rxmarbles.com/"
+    name: 'RxMarbles',
+    description: 'Interactive diagrams of Rx Observables',
+    repository: 'https://github.com/staltz/rxmarbles',
+    homepage: 'http://rxmarbles.com/',
+    screenshot: 'data/images/rxmarbles.png'
+  },
+  {
+    name: "Ghostwriter",
+    description: "Autocompleting rap/rhyme editor",
+    repository: "https://github.com/widdershin/ghostwriter",
+    homepage: "http://widdersh.in/ghostwriter"
+    screenshot: "data/images/ghostwriter.png"
   }
 ]
