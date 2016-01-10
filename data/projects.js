64401b9c8f2afe2dcf10b72713e8fb42e48339c6
Nick Johnstone <ncwjohnstone@gmail.com>
2016-01-10 14:30:04 +1300

    Add tricycle and cycle music sequencer

--- data/projects.js
+++ data/projects.js
@@ -1,10 +1,17 @@
 export default [
   {
-    name: 'RxMarbles',
-    description: 'Interactive diagrams of Rx Observables',
-    repository: 'https://github.com/staltz/rxmarbles',
-    homepage: 'http://rxmarbles.com/',
-    screenshot: 'data/images/rxmarbles.png'
+    name: 'Tricycle',
+    description: 'Try Cycle.js in your browser, no setup required',
+    repository: 'https://github.com/widdershin/tricycle/',
+    homepage: 'http://widdersh.in/tricycle',
+    screenshot: 'data/images/tricycle.png'
+  },
+  {
+    name: 'Cycle Music Sequencer',
+    description: 'An easy way to make melodies in your browser, powered by Cycle.js and Tone.js',
+    repository: 'https://github.com/widdershin/cycle-music-sequencer/',
+    homepage: 'http://widdersh.in/cycle-music-sequencer/',
+    screenshot: 'data/images/cycle-music-sequencer.png'
   },
   {
     name: 'Ghostwriter',
@@ -12,5 +19,12 @@ export default [
     repository: 'https://github.com/widdershin/ghostwriter',
     homepage: 'http://widdersh.in/ghostwriter',
     screenshot: 'data/images/ghostwriter.png'
+  },
+  {
+    name: 'RxMarbles',
+    description: 'Interactive diagrams of Rx Observables',
+    repository: 'https://github.com/staltz/rxmarbles',
+    homepage: 'http://rxmarbles.com/',
+    screenshot: 'data/images/rxmarbles.png'
   }
 ]
